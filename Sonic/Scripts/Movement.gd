7ded23d4a041fa0fbf93f96b70a6715fc0c376bd
ChezyName <mineboy2468@gmail.com>
2023-04-04 20:52:11 -0500

    added sonic jumping

--- Sonic/Scripts/Movement.gd
+++ Sonic/Scripts/Movement.gd
@@ -3,32 +3,42 @@ extends RigidBody3D
 const SpinSpeed = 16;
 const GroundSpeed = 24;
 const AirSpeed = 14;
-const SlowDownPercentage = 0.15;
+const SlowDownPercentage = 0.75;
 const AirControlPercentage = 0.6;
 
 var facingForward = true;
 var ForceGravity = 0;
 
 var SonicMesh:Node3D;
+var MeshHolder:Node3D;
 var GroundCheck:RayCast3D;
 var Animator:AnimationPlayer;
+var Spedometer:RichTextLabel;
 
 @export var SpeedMS:int = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	SonicMesh = get_node("SonicMesh");
+	SonicMesh = get_node("MeshHolder/SonicMesh");
 	GroundCheck = get_node("GroundCheck")
-	Animator = get_node("SonicMesh/AnimationPlayer")
+	Animator = get_node("MeshHolder/SonicMesh/AnimationPlayer")
+	Spedometer = get_node("HUD/Speed")
+	MeshHolder = get_node("MeshHolder")
 	pass # Replace with function body.
 
 # process velocity and play animation based on that
 func PlayAnimation(velocity,OnGround) -> void:
-	Animator.speed_scale = abs(velocity.z) / 10
-	if(abs(velocity.z) > 60): Animator.play("sc_boost");
+	Animator.speed_scale = abs(velocity.z) / 10;
+	# Broke Sound Barrier
+	if(abs(velocity.z) > 120): Animator.play("sc_boost");
 	elif(abs(velocity.z) > 0): Animator.play("sc_run");
-	elif(!OnGround): Animator.play("sc_jump_ball");
 	else: Animator.play("");
+	
+	if(!OnGround): 
+		Animator.play("sc_jump_ball");
+		MeshHolder.rotation_degrees.x += 9999;
+	else:
+		MeshHolder.rotation_degrees.x = 0;
 	
 	#print(velocity)
 
@@ -43,25 +53,25 @@ func _process(delta):
 	if(abs(MoveInput.x) > 0):
 		if(abs(c_Vel.z) > 0.5): XInput = MoveInput.x;
 		elif((c_Vel.z > 0.5 and MoveInput.x <= -0.5) or (c_Vel.z < -0.5 and MoveInput.x >= 0.5)):
-			XInput = -c_Vel.z*(SlowDownPercentage);
+			XInput = -((c_Vel.z * SlowDownPercentage) * 2);
 		elif(c_Vel.z <= 0.5 and MoveInput.x <= -0.5):
 			XInput = -1;
 			facingForward = false;
 		else: facingForward = true;
-	elif (MoveInput.x == 0 and abs(c_Vel.z) > 0):
-		MoveInput.x = -c_Vel.z*SlowDownPercentage;
+	#elif (MoveInput.x == 0 and abs(c_Vel.z) > 0):
+		#XInput = -(c_Vel.z * (SlowDownPercentage*SlowDownPercentage));
 	
 	c_Vel.z += (XInput * GroundSpeed * delta);
 	
 	if isOnGround:
 		if(Input.is_action_just_pressed("Jump")): c_Vel.y = 6;
 	else:
-		c_Vel.y -= 9.8 * delta;
+		if(Input.is_action_pressed("Jump")): c_Vel.y -= 5.8 * delta;
+		else: c_Vel.y -= 9.8 * delta;
 	
 	#Set Velecity
 	self.linear_velocity = c_Vel;
-	SpeedMS = abs(c_Vel.z);
-	print(str(SpeedMS) + " m/s");
+	Spedometer.text = str(int(abs(c_Vel.z))) + "m/s";
 	PlayAnimation(c_Vel,isOnGround)
 	
 	#Flip Character Based On facingForward
