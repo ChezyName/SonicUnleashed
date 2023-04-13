9b0b371bb23456d75d24ab1b09edee34b06739f3
ChezyName <mineboy2468@gmail.com>
2023-04-12 20:07:40 -0500

    Added Distructable Rock

---
+++ Rock/distructablerock.gd
@@ -0,0 +1,21 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+var destroying = false
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(destroying == true): return
+	for body in get_overlapping_bodies():
+		if(body.name == "SonicPlayer"):
+			if(abs(body.linear_velocity.z) > 12 and body.Rolling):
+				destroying = true
+				$RockCollider.queue_free()
+				$BreakSFX.play()
+				await $BreakSFX.finished
+				queue_free()
+	pass
